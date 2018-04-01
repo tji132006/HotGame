8628e860325fab65df124049c0f056e629776ffa
unknown <tji132006@gmail.com>
2018-04-01 11:27:56 -0700

    built project footer files

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from '../assets/images/logo.svg';
 import '../styles/App.css';
 import {EventHeader} from "./EventHeader"
+import {EventFooter} from "./EventFooter"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <EventHeader/>
+          <EventFooter/>
       </div>
     );
   }
