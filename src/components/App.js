d1bdbc665a77a62075c2e78abb510a037504e35e
unknown <tji132006@gmail.com>
2018-04-01 11:22:44 -0700

    built project header file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../assets/images/logo.svg';
 import '../styles/App.css';
+import {EventHeader} from "./EventHeader"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventHeader/>
       </div>
     );
   }
